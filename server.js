c86bdfbe56b926bca29bb35b2a734e848f67633d
Wei-Chung Chen <wcchen101@gmail.com>
2018-09-09 22:19:25 +0800

    add & update: add addblock api and update getblock api, and add error handling for addblock and getblock function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const Hapi = require('hapi');
 
 const Blockchain = require('./simpleChain');
+const Block = require('./simpleBlock');
 
 // Create a server with a host and port
 const server = Hapi.server({
@@ -26,8 +27,50 @@ server.route({
     method: 'GET',
     path: '/block/{id}',
     handler: (request, h) => {
-      return blockchain.getBlock(request.params.id)
-           .then((res) => res)
+      return new Promise((resolve, reject) => {
+        let blockHeight = blockchain.blockHeight
+
+        // check if id greater than block height
+        if (request.params.id > blockHeight) {
+          console.log('error')
+          resolve('error')
+        } else {
+          //ok to get block
+          try {
+            blockchain.getBlock(request.params.id).then((res) => resolve(res));
+          } catch(err) {
+            reject('error');
+          }
+        }
+      });
+    }
+});
+
+// api for post new block
+server.route({
+    method: 'POST',
+    path: '/block',
+    handler: (request, h) => {
+      try {
+        let payloadParsed = JSON.parse(request.payload);
+        console.log(payloadParsed, typeof payloadParsed);
+        if (payloadParsed.body != undefined && payloadParsed.body !== '') {
+          let newBlock = new Block(payloadParsed.body);
+          console.log('new block', newBlock)
+          console.log('success')
+          return new Promise((resolve, reject) => {
+            blockchain.addBlock(newBlock)
+                      .then(resolve('success'))
+                      .catch(reject('error'))
+          });
+        } else {
+          console.log('error here');
+          return 'error';
+        }
+      } catch(err) {
+        console.log(err);
+        return 'error';
+      }
     }
 });
 
