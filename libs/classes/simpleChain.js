2cc148297af06f7ad3e8f42b947698fdca5938da
Wei-Chung Chen <wcchen101@gmail.com>
2018-09-24 13:30:00 +0800

    update: move all functions and classes file in libs folder

---
+++ libs/classes/simpleChain.js
@@ -0,0 +1,195 @@
+/* ===== SHA256 with Crypto-js ===============================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
+|  =========================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+const levelSandbox = require('../functions/levelSandbox');
+
+const Block = require('./simpleBlock');
+// import Block from './simpleBlock';
+// /* ===== Block Class ==============================
+// |  Class with a constructor for block          |
+// |  ===============================================*/
+//
+// class Block{
+//   constructor(data){
+//      this.hash = "",
+//      this.height = 0,
+//      this.body = data,
+//      this.time = 0,
+//      this.previousBlockHash = ""
+//   }
+// }
+
+/* ===== Blockchain Class ==========================
+|  Class with a constructor for new blockchain    |
+|  ================================================*/
+
+module.exports = class Blockchain{
+  constructor(){
+		// this.chain = [];
+    this.blockHeight;
+		levelSandbox.getAllLevelDBData().then((height) => {
+			if (height == 0) {
+				let genesisBlock = new Block("First block in the chain - Genesis block")
+				this.addBlock(genesisBlock)
+			} else {
+        this.getBlockHeight().then((height) => {
+          this.blockHeight = height
+        });
+      }
+		});
+  }
+
+  // Add new block
+  async addBlock(newBlock){
+    // Block height
+    // newBlock.height = this.chain.length;
+    return new Promise(async (resolve, reject) => {
+      let previousBlock;
+      let chainLength;
+      if (newBlock.body !== 'First block in the chain - Genesis block') {
+        await this.getBlockHeight().then((height) => {
+          newBlock.height = height + 1
+          chainLength = height + 1
+
+          //add to blockHeight
+          this.blockHeight = height + 1
+        });
+        console.log('chainLength', chainLength)
+        await this.getBlock(chainLength-1).then((block) => {
+          previousBlock = block
+        })
+      }
+      // UTC timestamp
+      newBlock.time = new Date().getTime().toString().slice(0,-3);
+      // previous block hash
+      if(chainLength > 0){
+        newBlock.previousBlockHash = previousBlock.hash;
+      }
+      // Block hash with SHA256 using newBlock and converting to a string
+      newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+      // Adding block object to chain
+      // this.chain.push(newBlock);
+      levelSandbox.addLevelDBData(newBlock.height, JSON.stringify(newBlock)).then((data) => console.log(data))
+      resolve(newBlock.height)
+    });
+  }
+
+  // Get block height
+	async getBlockHeight(){
+    return await new Promise((resolve) => {
+			levelSandbox.getAllLevelDBData().then((height) => {
+				console.log('height', height)
+				resolve(height)
+			});
+		});
+  }
+
+  // get block
+  async getBlock(blockHeight){
+    return await new Promise((resolve, reject) => {
+      levelSandbox.getLevelDBData(blockHeight).then((block) => {
+        try {
+          resolve(block)
+        } catch (err) {
+          reject(err)
+        }
+      });
+    });
+  }
+
+  // validate block
+  async validateBlock(blockHeight){
+    // get block object
+		return await new Promise((resolve, reject) => {
+			levelSandbox.getLevelDBData(blockHeight).then((block) => {
+				try {
+					// get block hash
+					let blockHash = block.hash;
+					// remove block hash to test block integrity
+					block.hash = '';
+					// generate block hash
+					let validBlockHash = SHA256(JSON.stringify(block)).toString();
+					// Compare
+					if (blockHash===validBlockHash) {
+						console.log('passed validation! ')
+						resolve(true)
+					} else {
+						console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
+						resolve(false)
+					}
+				} catch (err) {
+					reject(err)
+				}
+			});
+		});
+  }
+
+ // Validate blockchain
+  async validateChain(){
+    let errorLog = [];
+    let promiseBlock;
+    let promisePreBlock;
+    let promiseCurBlock;
+    let chainLength;
+    await this.getBlockHeight().then((height) => {
+      chainLength = height + 1
+    });
+
+    let promiseValidation = await new Promise((resolve, reject) => {
+      for (let i = 0; i < chainLength; i++) {
+          if (i == 0) {
+            continue;
+          }
+
+          let preBlock;
+          let curBlock;
+          promisePreBlock = this.getBlock(i - 1).then((block) => {
+            preBlock = block
+
+          });
+          promiseCurBlock = this.getBlock(i).then((block) => {
+            curBlock = block
+          });
+
+          promiseBlock = this.validateBlock(i).then((isBlockValidation) => {
+            // validate block
+    				console.log(isBlockValidation)
+            if (!isBlockValidation)errorLog.push(i);
+
+            // compare blocks hash link
+    				console.log('hash', preBlock.hash)
+    				console.log('previous hash', curBlock.previousBlockHash)
+            let blockHash = preBlock.hash;
+            let previousHash = curBlock.previousBlockHash;
+            if (blockHash!==previousHash) {
+              //mark problematic block as error block
+              curBlock.body = 'block error'
+              errorLog.push(i);
+              reject(false);
+            }
+          });
+        resolve(true);
+      }
+    });
+
+    let res = await Promise.all([
+      promiseValidation,
+      promiseBlock,
+      promisePreBlock,
+      promiseCurBlock
+    ]);
+
+    console.log('-----***** The validation result is below *****-----');
+    if (errorLog.length>0) {
+      console.log('***** The validation result is not passed! *****');
+      console.log('Block errors = ' + errorLog.length);
+      console.log('Blocks: '+ errorLog);
+    } else {
+      console.log('***** The validation result is passed! *****');
+      console.log('No errors detected');
+    }
+  }
+}
---
+++ libs/functions/common.js
@@ -0,0 +1,107 @@
+let bitcoin = require('bitcoinjs-lib') // v3.x.x
+let bitcoinMessage = require('bitcoinjs-message')
+let fs = require('fs');
+
+let getCurrentTime = function () {
+  let date = new Date()
+  let curTimestamp = date.getTime()
+  return curTimestamp
+};
+
+let generateMessage = function (address, timestamp, message) {
+  return address + ':' + timestamp + ':' + message
+};
+
+let setReqValResponse = function (address, requestTimeStamp, message, validationWindow) {
+  let response = {
+    "address": address,
+    "requestTimeStamp": requestTimeStamp,
+    "message": message,
+    "validationWindow": validationWindow
+  }
+  return response
+}
+
+let setValidationResponse = function (address, requestTimeStamp, message, isValid) {
+  let curTimestamp = getCurrentTime()
+  let remainingTimeWindow = curTimestamp - requestTimeStamp
+  let response = {
+    "registerStar": isValid,
+    "status": {
+      "address": address,
+      "requestTimeStamp": requestTimeStamp,
+      "message": message
+    },
+    "validationWindow": remainingTimeWindow,
+    "messageSignature": isValid
+  }
+  return response
+}
+
+let setResInRedis = function (client, key, res, validationWindow) {
+  let resStr = JSON.stringify(res)
+  client.set(key, resStr);
+
+  // modify time to validationWindow for prod purpose
+  client.expire(key, validationWindow);
+  console.log('set into redis successfully for ' + validationWindow + ' seconds')
+  return 'success'
+}
+
+let getResInRedis = function (client, key) {
+  return new Promise((resolve, reject) => {
+    client.get(key, function(err, res) {
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      console.log('get successfully', res)
+      resolve(res)
+    });
+  })
+};
+
+let checkIsSignatureValidate = function(message, address, signature) {
+  return new Promise((resolve, reject) => {
+    let isValid = bitcoinMessage.verify(message, address, signature)
+
+    if (isValid) {
+      return resolve(true)
+    }
+
+    return resolve(false)
+  });
+};
+
+let setErrorMessage = function(status, error) {
+  let response = {
+    "statusCode": status,
+    "error": error,
+    "message": "Cannot find pages or internal server error. Please check the status code"
+  }
+  return response
+};
+
+let getRemainingTime = function(pastTimestamp, validationWindow) {
+  let currentTimestap = getCurrentTime()
+  let remainingTimeWindow = Number(pastTimestamp) + Number(validationWindow) * 1000 - Number(currentTimestap)
+  return remainingTimeWindow
+}
+
+let encodedWithHex = function(value) {
+  const buf = Buffer.from(value, 'ascii');
+  let encodedValue = buf.toString('hex')
+  console.log(encodedValue)
+  return encodedValue
+}
+
+exports.getCurrentTime = getCurrentTime;
+exports.generateMessage = generateMessage;
+exports.setReqValResponse = setReqValResponse;
+exports.setResInRedis = setResInRedis;
+exports.getResInRedis = getResInRedis;
+exports.checkIsSignatureValidate = checkIsSignatureValidate;
+exports.setValidationResponse = setValidationResponse;
+exports.setErrorMessage = setErrorMessage;
+exports.getRemainingTime = getRemainingTime;
+exports.encodedWithHex = encodedWithHex;
