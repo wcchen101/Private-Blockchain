5f58cbbde2c7a2f6cce9e85e7342dfd3fa4d5cdc
Wei-Chung Chen <wcchen101@gmail.com>
2018-10-26 23:30:15 +0800

    add: star related smart contract, mint, string compare smart contract

--- smart-contracts/Project5-smartContract-with-Zeppelin/smart_contracts/contracts/StarNotary.sol
+++ smart-contracts/Project5-smartContract-with-Zeppelin/smart_contracts/contracts/StarNotary.sol
@@ -5,14 +5,31 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 contract StarNotary is ERC721 {
 
     struct Star {
-        string name; 
+        string name;
+        string starCoordinators;
+        string story;
     }
 
     mapping(uint256 => Star) public tokenIdToStarInfo;
     mapping(uint256 => uint256) public starsForSale;
+    uint256[] public tokenIdIndices;
 
-    function createStar(string _name, uint256 _tokenId) public {
-        Star memory newStar = Star(_name);
+    function createStar(string _name, string _dec, string _mag, string _cent, string _story, uint256 _tokenId) public {
+        //to check if star coordinators duplicate beforehand
+        string memory starCoordinators = starCoordinatorsConcat(_dec, _mag, _cent);
+
+        // can put int to a function
+        for (uint i = 0; i < tokenIdIndices.length; i++) {
+          uint tokenId = tokenIdIndices[i];
+          Star storage star = tokenIdToStarInfo[tokenId];
+          bool result = isStringsEqual(starCoordinators, star.starCoordinators);
+          require(result != true);
+        }
+
+        //add tokenId into tokenIdIndices array
+        tokenIdIndices.push(_tokenId);
+
+        Star memory newStar = Star(_name, starCoordinators, _story);
 
         tokenIdToStarInfo[_tokenId] = newStar;
 
@@ -40,5 +57,57 @@ contract StarNotary is ERC721 {
         if(msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
+    }
+
+    function starCoordinatorsConcat(string _dec, string _mag, string _cent) public returns (string) {
+      bytes memory _decBytes = bytes(_dec);
+      bytes memory _magBytes = bytes(_mag);
+      bytes memory _centBytes = bytes(_cent);
+      string memory starCoordinatorsStr = new string(_decBytes.length + _magBytes.length + _centBytes.length);
+      bytes memory starCoordinatorsBytes = bytes(starCoordinatorsStr);
+      uint j = 0;
+      for (uint i = 0; i < _decBytes.length; i++) starCoordinatorsBytes[j++]  = _decBytes[i];
+      for (i = 0; i < _magBytes.length; i++) starCoordinatorsBytes[j++] = _magBytes[i];
+      for (i = 0; i < _centBytes.length; i++) starCoordinatorsBytes[j++] = _centBytes[i];
+      return string(starCoordinatorsStr);
+    }
+
+    function checkStarExist(string _dec, string _mag, string _cent) internal returns (bool) {
+      string memory starCoordinators = starCoordinatorsConcat(_dec, _mag, _cent);
+      for (uint i = 0; i < tokenIdIndices.length; i++) {
+        if (isStringsEqual(starCoordinators, tokenIdToStarInfo[tokenIdIndices[i]].starCoordinators)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function checkIfStarExist(string _dec, string _mag, string _cent) public view returns (bool) {
+      string memory starCoordinators = starCoordinatorsConcat(_dec, _mag, _cent);
+      for (uint i = 0; i < tokenIdIndices.length; i++) {
+        if (isStringsEqual(starCoordinators, tokenIdToStarInfo[tokenIdIndices[i]].starCoordinators)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function isStringsEqual(string _strA, string _strB) internal returns (bool) {
+      bytes memory strA = bytes(_strA);
+      bytes memory strB = bytes(_strB);
+      if (strA.length != strB.length) {
+        return false;
+      }
+
+      for (uint i = 0; i < strA.length; i++) {
+        if(strA[i] != strB[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function mint(uint256 _tokenId) public {
+      _mint(msg.sender, _tokenId);
     }
 }
